d318ae71efe87afd9460b50195039861d425f153
Chen <victorchennn@gmail.com>
2018-04-24 12:05:04 -0700

    add side bar to the widget

--- game/Widget.java
+++ game/Widget.java
@@ -14,19 +14,18 @@ public class Widget extends Pad{
         _size = size;
         _boardSide = size * TILE_SIDE_SEP + TILE_SEP_1;
         _tiles = new ArrayList<>();
-        setPreferredSize(_boardSide + 100, _boardSide);
+        setPreferredSize(_boardSide + TILE_SIDE * 2, _boardSide);
     }
 
     /** Add the value of tile TILE to the board. */
     private void draw_tile(Graphics2D g, Tile tile) {
         int col = tile.col();
         int row = tile.row();
-        int adjust = 16;
         g.setFont(TILE_FONT);
         String number = Integer.toString(tile.value());
 
-        int x_position = adjust + col * TILE_SIDE_SEP;
-        int y_position = _boardSide - row * TILE_SIDE_SEP - adjust;
+        int x_position = ADJUST + col * TILE_SIDE_SEP;
+        int y_position = _boardSide - row * TILE_SIDE_SEP - ADJUST;
         g.drawString(number, x_position, y_position);
     }
 
@@ -34,20 +33,31 @@ public class Widget extends Pad{
     public synchronized void paintComponent(Graphics2D g) {
         g.setColor(EMPTY_SQUARE_COLOR);
         g.fillRect(0,0, _boardSide, _boardSide);
+        g.fillRect(_boardSide + TILE_SIDE, 0, TILE_SIDE, _boardSide);
         g.setColor(BAR_COLOR);
         for (int k = 0, t = 0; k <= _boardSide; k += TILE_SIDE_SEP, t++) {
             g.fillRect(0, k, _boardSide, TILE_SEP_1);
             g.fillRect(k, 0, TILE_SEP_1, _boardSide);
+            g.fillRect(_boardSide + TILE_SIDE, k, TILE_SIDE, TILE_SEP_1);
             if (t == 3 || t == 6) {
                 g.fillRect(0, k, _boardSide, TILE_SEP_2);
                 g.fillRect(k, 0, TILE_SEP_2, _boardSide);
+                g.fillRect(_boardSide + TILE_SIDE, k, TILE_SIDE, TILE_SEP_2);
             }
         }
-        g.setColor(FONT_COLOR);
+        g.fillRect(_boardSide + TILE_SIDE, 0, TILE_SEP_1, _boardSide);
+        g.setColor(FONT_COLOR_1);
         for (Tile t : _tiles) {
             if (t.value() != 0) {
                 draw_tile(g, t);
             }
+        }
+        for (int i = 0; i < _size; i++) {
+            g.setColor(FONT_COLOR_2);
+            String number = Integer.toString(i + 1);
+            int x_position = ADJUST + _boardSide + TILE_SIDE;
+            int y_position = i * TILE_SIDE_SEP + TILE_SIDE - ADJUST + TILE_SEP_2;
+            g.drawString(number, x_position, y_position);
         }
     }
 
@@ -62,6 +72,7 @@ public class Widget extends Pad{
 
     /** Width of board and grid lines. */
     static final int
+        ADJUST = 16,
         TILE_SEP_1 = 2,
         TILE_SEP_2 = 5,
         TILE_SIDE = 50,
@@ -71,7 +82,8 @@ public class Widget extends Pad{
     static final Color
         EMPTY_SQUARE_COLOR = new Color(255, 255, 255),
         BAR_COLOR = new Color(140, 140, 140),
-        FONT_COLOR = new Color(0,0,0);
+        FONT_COLOR_1 = new Color(0,0,0),
+        FONT_COLOR_2 = new Color(0, 204, 255);
 
     /** Font of number. */
     static final Font TILE_FONT = new Font("Number", Font.ITALIC, 30);
